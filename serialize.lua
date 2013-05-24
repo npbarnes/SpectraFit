ccd2fda2f94d2017d4e49a287e38dd6e336b5df8
Nathan Barnes <npbarnes@coe.edu>
2013-05-24 00:36:06 -0500

    save and serialize work more or less how I want.

--- serialize.lua
+++ serialize.lua
@@ -1,5 +1,10 @@
-function basicSerialize (name, value)
-    file:write(name, " = ")
+--[[
+Code found in the Lua textbook, more general than the code
+found below, but I only intend to use simple tables so I'll go with
+a less general but more readable solution
+--]]
+--[[
+function basicSerialize (value)
     if type(value) == "number" then
         return tostring(value)
     elseif type(value) == "string" then
@@ -9,47 +14,63 @@ function basicSerialize (name, value)
     end
 end
 
--- this implementation will only work for tables whose indecies are
--- all strings or numbers and whose subtables (and subsubtables...)
--- meet the same criteria
-function tableSerialize(name,value,saved)
-    if type(value) ~= "table" then
-        error("tableSerialize is only for tables",2)
-    end
-
-    local ret = name .. " = "
-
-    -- the `saved' table is indexed by table references
-    if saved[value] then -- if this subtable has been saved
-        ret = ret .. saved[value] .. "\n"
-    else
-        saved[value] = name
-        ret = ret .. "{}\n"
-        for k,v in pairs(value) do -- save its fields
-            -- this line is why you can't serialize tables
-            -- indexed with tables (see basicSerialize)
-            local fieldname =
-                    string.format("%s[%s]",name,basicSerialize(k))
-            ret = ret .. serialize(fieldname,v,saved)
-        end
-    end
-
-    return ret
-end
-
-function serialize(name,value,saved)
-    saved = saved or {}     -- initial value
+function save (name, value, saved)
+    saved = saved or {}       -- initial value
+    io.write(name, " = ")
     if type(value) == "number" or type(value) == "string" then
-        return basicSerialize(name, value) .. "\n"
+        io.write(basicSerialize(value), "\n")
     elseif type(value) == "table" then
-        return tableSerialize(name, value, saved) .. "\n"
+        if saved[value] then    -- value already saved?
+            io.write(saved[value], "\n")  -- use its previous name
+        else
+            saved[value] = name   -- save name for next time
+            io.write("{}\n")     -- create a new table
+            for k,v in pairs(value) do      -- save its fields
+                local fieldname = string.format("%s[%s]", name,
+                basicSerialize(k))
+                save(fieldname, v, saved)
+            end
+        end
     else
-        error("cannot serialize a " .. type(value))
+        error("cannot save a " .. type(value))
+    end
+end
+--]]
+
+function iwrite(num, ...)
+    for i=num,1,-1 do
+        io.write("\t")
+    end
+    if ... then
+        io.write(...)
     end
 end
 
+function serialize (item,indent)
+    indent = indent or 0
 
-function save (name,value,file)
-    file = file or io.stdio
-    file:write( serialize(name,value),"\n")
+    if type(item) == "number" then
+        io.write(item)
+    elseif type(item) == "string" then
+        io.write(string.format("%q", item))
+    elseif type(item) == "table" then -- assume no shared subtables
+                                      -- and no cycles in the table
+        io.write("{\n")
+        for k,v in pairs(item) do
+            iwrite(indent, "\t[")
+            serialize(k)
+            io.write("] = ")
+            serialize(v,indent+1)
+            io.write(",\n")
+        end
+        iwrite(indent,"}")
+    else
+        error("cannot serialize a " .. type(item))
+    end
+end
+
+function save (name, item)
+    io.write(name, " = ")
+    serialize(item)
+    io.write("\n")
 end
