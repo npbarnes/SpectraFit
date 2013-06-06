74fcf059b1d176f6ab48ab051cd4e6ce6f9ac103
Nathan Barnes <npbarnes@coe.edu>
2013-06-06 11:11:01 -0300

    reorganized,into three separate algorithms

--- aldermanGrant.lua
+++ aldermanGrant.lua
@@ -1,3 +1,13 @@
+--[[
+information on these algorithms can be found in the following paper
+
+1986 D.W. Alderman et al, "Methods for Analyzing Spectroscopic Line
+Shapes.  NMR Solid Powder Patterns" J. Chem. Phys., Vol. 84, No. 7
+--]]
+
+-- A table to hold Alderman-Grant algorithms
+AG = {}
+
 local N = State.simParam.AldermanGrantN
 local bins = State.ioSettings.bins
 local freqFunc = freqFunc or function (cosTheta,cos2Phi,...)
@@ -9,19 +19,26 @@ local larmor = State.physParam.larmor
 local bins = Sate.ioSettings.bins
 local I = State.physParam.spin
 
--- returns the list of "tents" as described in the paper below that
--- can then be converted into a spectrum histogram
--- 1986 D.W. Alderman et al, "Methods for Analyzing Spectroscopic Line
--- Shapes.  NMR Solid Powder Patterns" J. Chem. Phys., Vol. 84, No. 7
-function aldermanGrant()
+-- returns the list of frequencies that can then be used to create the
+-- "tents" that will finally be used to create a spectrum histogram
+-- frequencies will be in the form of a 2-d array indexed so that
+-- freq[i][j] will give you the frequency calculated at the point
+-- (i,j) on the octohedron described in the paper
+function AG.frequencies()
     -- tents will be in the form
     -- {{high = 12.2,mid = 11.0,low = 9.8, weight = 3.2},...}
     local tents = {}
+
+    local frequencies = {}
 
     -- for each transition (i.e. each spectral line)
     -- m represents the transition from the magnetic quantum number m
     -- to m-1
     for m = -(I-1),I do
+        -- 'i' and 'j' are the indecies of the points on each face
+        for i=-N, N do
+            for j=-(N-math.abs(i)), N-math.abs(i) do
+            end
+        end
     end
-
 end
